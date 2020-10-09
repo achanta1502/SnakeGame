10881b5bc6cd33459cb4041d7469d833ac69b2c7
pavan kumar achanta <38113680+achanta1502@users.noreply.github.com>
2020-10-09 10:35:00 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 window.onload=function() {
     canv = document.getElementById("game");
     ctx = canv.getContext("2d");
-    snake_color = document.getElementById("snake").value;
-    dot_color = document.getElementById("dot").value;
-    speed = parseFloat(document.getElementById("speed").value)*10;
     document.getElementById("start").addEventListener("click", startGame);
 }
 
 function startGame() {
+    snake_color = document.getElementById("snake").value;
+    dot_color = document.getElementById("dot").value;
+    speed = parseFloat(document.getElementById("speed").value)*10;
     document.addEventListener("keydown", keyPush);
     setInterval(game, 1000/speed);
 }
 
 px = py = 10;   
-gs = tc = 20;
-ax = ay = 15;
+block = 20;
+dot_x = dot_y = 15;
 x=y=0;
 
 body = [];
@@ -24,23 +24,23 @@ function game() {
     px+=x;
     py+=y;
     if(px<0) {
-        px= tc-1;
+        px= block-1;
     }
-    if(px>tc-1) {
+    if(px>block-1) {
         px= 0;
     }
     if(py<0) {
-        py= tc-1;
+        py= block-1;
     }
-    if(py>tc-1) {
+    if(py>block-1) {
         py= 0;
     }
     ctx.fillStyle="black";
-    ctx.fillRect(0, 0, canv.width, canv.height);400
+    ctx.fillRect(0, 0, canv.width, canv.height);
  
     ctx.fillStyle=snake_color;
     for(var i=0;i<body.length;i++) {
-        ctx.fillRect(body[i].x*gs,body[i].y*gs,gs-2,gs-2);
+        ctx.fillRect(body[i].x*block,body[i].y*block,block-2,block-2);
         if(body[i].x==px && body[i].y==py) {
             tail = 5;
         }
@@ -50,13 +50,13 @@ function game() {
     body.shift();
     }
  
-    if(ax==px && ay==py) {
+    if(dot_x==px && dot_y==py) {
         tail++;
-        ax=Math.floor(Math.random()*tc);
-        ay=Math.floor(Math.random()*tc);
+        dot_x=Math.floor(Math.random()*block);
+        dot_y=Math.floor(Math.random()*block);
     }
     ctx.fillStyle=dot_color;
-    ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
+    ctx.fillRect(dot_x*block,dot_y*block,block-2,block-2);
 }
 function keyPush(event) {
     switch(event.keyCode) {
